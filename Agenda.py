efd05ace0077b0cf362d50d8e2ffc34bbcfe4bed
Pelé <ryansalesj4@gmail.com>
2023-08-26 21:49:39 -0300

    Update Agenda.py

--- Agenda.py
+++ Agenda.py
@@ -3,7 +3,19 @@ from tkinter.ttk import*
 from tkinter import*
 from tkinter import messagebox
 
+# Variaveis das cores
 
+co0 = "#f0f3f5" # preto
+co1 = "#f0f3f5" # cinza
+
+# Criação da janela
+janela = Tk()
+janela.title("Agenda")
+janela.configure(background=co1)
+janela.resizable(width=FALSE, height=FALSE)
+
+style = Style(janela)
+style.theme_use("clam")
 
 nome = []
 telefone = []
