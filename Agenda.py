2674ed5048e16ffae2f5317f22e6ec7ace362a25
Pelé <ryansalesj4@gmail.com>
2023-08-26 22:02:27 -0300

    Update Agenda.py

--- Agenda.py
+++ Agenda.py
@@ -7,11 +7,12 @@ from tkinter import messagebox
 
 co0 = "#f0f3f5" # preto
 co1 = "#f0f3f5" # cinza
+co2 = "#feffff" # branco
 co3 = "#38576b" # black
 
 # Criação da janela
 janela = Tk()
-janela.title("")
+janela.title("Agenda")
 janela.configure(background=co1)
 janela.resizable(width=FALSE, height=FALSE)
 
@@ -24,15 +25,13 @@ frame_cima = Frame(janela, width=500, height=50, bg=co3, relief="flat")
 frame_cima.grid(row=0, column=0, pady=1, padx=0, sticky=NSEW)
 
 frame_baixo = Frame(janela, width=500, height=150, bg=co1, relief="flat")
+frame_baixo.grid(row=1, column=0, pady=1, padx=0, sticky=NSEW)
 
+frame_tabela = Frame(janela, width=500, height=240, bg=co2, relief="flat")
+frame_tabela.grid(row=2, column=0, columnspan=2, padx=10,pady=1, sticky=NSEW)
+
+janela.mainloop()
 # 
 
-nome = []
-telefone = []
 
-x = 0
-
-def salvarContato():
-    nome[x] = input("Digite um nome: \n")
-    telefone[x] = input("Digite um telefone: \n")
 
