9c374100bebd4e9c23e6c3d733d451889d2f5450
Pelé <ryansalesj4@gmail.com>
2023-08-26 21:57:27 -0300

    Update Agenda.py

--- Agenda.py
+++ Agenda.py
@@ -7,15 +7,25 @@ from tkinter import messagebox
 
 co0 = "#f0f3f5" # preto
 co1 = "#f0f3f5" # cinza
+co3 = "#38576b" # black
 
 # Criação da janela
 janela = Tk()
-janela.title("Agenda")
+janela.title("")
 janela.configure(background=co1)
 janela.resizable(width=FALSE, height=FALSE)
 
 style = Style(janela)
 style.theme_use("clam")
+
+# Frames
+
+frame_cima = Frame(janela, width=500, height=50, bg=co3, relief="flat")
+frame_cima.grid(row=0, column=0, pady=1, padx=0, sticky=NSEW)
+
+frame_baixo = Frame(janela, width=500, height=150, bg=co1, relief="flat")
+
+# 
 
 nome = []
 telefone = []
