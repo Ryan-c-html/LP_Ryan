3d75c2e21fc2dae7683c9c47f1987bb88102dddb
Pelé <ryansalesj4@gmail.com>
2023-08-28 20:00:11 -0300

    .

--- Agenda.py
+++ Agenda.py
@@ -35,20 +35,20 @@ frame_tabela.grid(row=2, column=0, columnspan=2, padx=10,pady=1, sticky=NSEW)
 
 # Configurando frame cima
 
-l_nome = Label(frame_cima, text="Lista telefônica", anchor=NE, font=('times 20 bold'), bg=co3, fg=co1)
-l_nome.place(x=10, y=5)
+l_linha = Label(frame_cima, text="Lista telefônica", anchor=NE, font=('times 20 bold'), bg=co3, fg=co1)
+l_linha.place(x=10, y=5)
 
 # Configurando frame do meio
 
-l_linha = Label(frame_baixo, text="Nome:*", anchor=NW, font=("arial 12 bold"),bg=co1, fg=co3)
-l_linha.place(x=20, y=10)
-e_linha = Entry(frame_baixo, width=25, justify='left', font=('',10), highlightthickness=1)
-e_linha.place(x=110, y=10)
+l_nome = Label(frame_baixo, text="Nome:*", anchor=NW, font=("arial 12 bold"),bg=co1, fg=co3)
+l_nome.place(x=20, y=10)
+e_nome = Entry(frame_baixo, width=25, justify='left', font=('',10), highlightthickness=1)
+e_nome.place(x=110, y=10)
 
-l_linha1 = Label(frame_baixo, text="Email:*", anchor=NW, font=("arial 12 bold"),bg=co1, fg=co3)
-l_linha1.place(x=20, y=32)
+l_email = Label(frame_baixo, text="Email:*", anchor=NW, font=("arial 12 bold"),bg=co1, fg=co3)
+l_email.place(x=20, y=32)
 
-l_linha2 = Label(frame_baixo, text="Telefone:*", anchor=NW, font=("arial 12 bold"),bg=co1, fg=co3)
-l_linha2.place(x=20, y=56)
+l_tel = Label(frame_baixo, text="Telefone:*", anchor=NW, font=("arial 12 bold"),bg=co1, fg=co3)
+l_tel.place(x=20, y=56)
 
 janela.mainloop()
