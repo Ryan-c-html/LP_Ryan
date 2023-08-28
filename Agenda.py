89ff9f16bbc67aa8531eff4cbf557007db0833e4
Pelé <ryansalesj4@gmail.com>
2023-08-28 20:21:10 -0300

    .

--- Agenda.py
+++ Agenda.py
@@ -46,9 +46,13 @@ e_nome = Entry(frame_baixo, width=25, justify='left', font=('',10), highlightthi
 e_nome.place(x=110, y=10)
 
 l_email = Label(frame_baixo, text="Email:*", anchor=NW, font=("arial 12 bold"),bg=co1, fg=co3)
-l_email.place(x=20, y=32)
+l_email.place(x=20, y=35)
+e_email = Entry(frame_baixo, width=25, justify='center', font=('',10), highlightthickness=1)
+e_email.place(x=110, y=35)
 
 l_tel = Label(frame_baixo, text="Telefone:*", anchor=NW, font=("arial 12 bold"),bg=co1, fg=co3)
 l_tel.place(x=20, y=56)
+e_tel = Label(frame_baixo, width=25, justify='center', font=('',10), highlightthickness=1)
+e_tel.place(x=110, y=56)
 
 janela.mainloop()
