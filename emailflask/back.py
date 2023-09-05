5d644da2f63431e5a58ba140a5ec4bf2dc15f0e4
Pelé <ryansalesj4@gmail.com>
2023-09-05 14:52:39 -0300

    O codigo está com erro, porém as funções ainda não foram criadas do modo certo

--- emailflask/back.py
+++ emailflask/back.py
@@ -3,9 +3,19 @@ from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 # Página de login
-@app.route('/')
-def login():
+@app.route('/main', methods['GET'])
+def main():
+    if request.method == 'POST':
+        name = request.form.get("botao")
+        if name == 'logar':
+            render_template('login.html')
+        elif name == 'cadastro':
+            render_template('cadastro.html')
     return render_template('main.html')
+
+@app.route('/login', methods=['gets'])
+def login():
+    return render_template('login.html')
 
 # Página após o login
 @app.route('/pagina_dados', methods=['GET', 'POST'])
@@ -18,12 +28,12 @@ def pagina_dados():
 
         filename = f"{data}_{destinatario}.txt"
         with open(filename, 'w') as f:
-            f.write(f"Data: {data}\nDestinatário: {destinatario}\nMensagem: {mensagem}\nRemetente: {remetente}")
+            f.write(f"Data: {data}\nDestinatario: {destinatario}\nMensagem: {mensagem}\nRemetente: {remetente}")
 
     return render_template('pagina_dados.html')
 
-@app.route('/cadastro')
+@app.route('/cadastro', methods=['POST', 'POST'])
 def cadastro():
-    return 0
+        return render_template('cadastro.html')
 
 app.run()
