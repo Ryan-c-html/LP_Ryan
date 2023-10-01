cf795759fff341af81ecade8b6feeacc6c2c2944
Ryan <ryansalesj4@gmail.com>
2023-09-30 23:52:27 -0300

    MODIFIED - Modificações para deixar o repositorio mais clean

---
+++ Aulas/aula_17-08.py
@@ -0,0 +1,30 @@
+# Codigo de uma lista telefonica
+x = 0
+
+nome = []
+telefone = []
+
+def novoContato():
+    nome[x] = input("Digite um nome: \n")
+    telefone[x] = input("Digite um telefone: \n")
+    x = x + 1
+
+def editarContato():
+    a = input("Qual contato você deseja editar? \n")
+    print("Você deseja editar o contato do(a) " + nome[a] + "?\n")
+    l = input("Se sim escreva 's', e se não escreva 'n'")
+
+    print()
+
+def removerContato():
+    a = input("Deseja retirar qual contato da sua agenda?")
+    print("Você deseja retirar o contato do(a) " + nome[a] + "?\n")
+    l = input("Se sim escreva 's', e se não escreva 'n'")
+
+def verContato():
+    pass
+
+def Terminar():
+    
+    pass
+
