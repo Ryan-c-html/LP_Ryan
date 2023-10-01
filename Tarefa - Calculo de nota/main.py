bd13654dacae2082fe714176b64be754bd068edd
Ryan <ryansalesj4@gmail.com>
2023-10-01 01:22:22 -0300

    MODIFIED - Todas as funções funcionando corretamente. Inclusive o banco de dados. Criação da página que irá redirecionar o professor.

--- Tarefa - Calculo de nota/main.py
+++ Tarefa - Calculo de nota/main.py
@@ -1,7 +1,7 @@
 # Inclusão das bibliotecas
 from flask import Flask, render_template, request, redirect, url_for
 # inclui a página criada ao programa principal e depois inclui suas funções
-from modelo import professor
+from modelo import professor, aluno
 
 app = Flask(__name__)
 
@@ -26,7 +26,7 @@ def login():
         loginNome = request.form['nome']
         loginSenha = request.form['senha']
         if professor1.verifica(loginNome, loginSenha):
-            return redirect("/tarefas")
+            return redirect("/redireciona")
         else:
             return redirect("/login")
     return render_template("login.html")
@@ -38,6 +38,10 @@ def tarefas():
 @app.route("/medias", methods=['POST', 'GET'])
 def medias():
     return render_template("medias.html")
+
+@app.route("/redireciona", methods=['POST', 'GET'])
+def redireciona():
+    return render_template("redirecionaProfessor.html")
 
 # Coloca o programa para rodar, tirar o "debug=True" qaundo o programa estiver finalizado
 app.run(debug=True)
--- Tarefa - Calculo de nota/modelo.py
+++ Tarefa - Calculo de nota/modelo.py
@@ -10,17 +10,37 @@ class professor():
     def verifica(self, nome, senha):
         self.nome = nome 
         self.senha = senha
-        with open("./logins.txt", "r") as txt:
-            linhas = txt.readlines()
-            for linha in linhas:
-                linha = linha.strip()
-                dados = linha.split("-")
-                if self.nome == dados[0] and self.senha == dados[1]:
-                    return True
-                #info.append(linha.split("-"))
-            return False
+        try:
+            with open("./Tarefa - Calculo de nota/logins.txt", "r") as txt:
+                linhas = txt.readlines()
+                for linha in linhas:
+                    linha = linha.strip()
+                    dados = linha.split("-")
+                    if len(dados) == 2 and self.nome == dados[0] and self.senha == dados[1]:
+                        return True
+        # Caso ocorra um erro ao encontrar o arquivo, as coisas que estão dentro dessa função começam a ser executadas
+        except FileNotFoundError:
+            # Essa parte cria um arquivo de login, caso o arquivo de login não existisse. Isso previne que ocorra erros 
+            # atrapalhem o programa.
+            with open("logins.txt", "w") as arquivo:
+                arquivo.write("Miceli-1234\nRyan-1234")
+            print("O arquivo 'logins.txt' não foi encontrado.")
+        return False
     def cadastro(self, nome, senha):
         self.nome = nome
         self.senha = senha
         with open("./logins.txt", "a") as txt:
             txt.write(f"{self.nome} - {self.senha}\n")
+
+class aluno():
+    def __init__(self):
+        pass
+    def calculaSituacao(self, t1, t2, l1, l2, l3, l4):
+        media = (((t1 + t2) * 0.8) + (((l1 + l2 + l3 + l4) / 4)*0.2))
+        if media >= 7:
+            situação = "aprovado"
+        elif 7 > media and media >= 3:
+            situação = "final"
+        else:
+            situação = "reprovado"
+        return situação
