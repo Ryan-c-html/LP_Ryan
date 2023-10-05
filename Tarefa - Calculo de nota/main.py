ea01e8238a9aa47559e4e17da3f6f74fd96a1cd5
arturgcr <arturgcr.20221@poli.ufrj.br>
2023-10-04 22:28:07 -0300

    DOCS e REFACTOR: documentação + refeita a lógica de uso das classes

--- Tarefa - Calculo de nota/main.py
+++ Tarefa - Calculo de nota/main.py
@@ -1,90 +1,140 @@
 # Inclusão das bibliotecas
 from flask import Flask, render_template, request, redirect, url_for
 # inclui a página criada ao programa principal e depois inclui suas funções
-from modelo import professor, aluno
+from modelo import Professor, Aluno
+# inclui as bibliotecas usadas para abrir automaticamente o projeto
+import webbrowser
+import threading
 
 app = Flask(__name__)
 
-professor1 = professor()
-aluno1 = aluno()
 
 # Página de inicio (Main)
 @app.route('/', methods=['POST', 'GET'])
 def inicio():
     if request.method == 'POST':
         botaoSelecionado = request.form['botao']
+        # Verifica se o botão apertado era o de login
         if botaoSelecionado == 'login':
             return redirect("/login")
+        # Verifica se o botão apertado era o de tarefas
         if botaoSelecionado == 'tarefas':
             return redirect("/tarefas")
+        # Verifica se o botão apertado era o de médias
         if botaoSelecionado == 'medias':
             return redirect("/medias")
+    # exibe a tela inicial
     return render_template("main.html")
 
+# Página de Login
 @app.route('/login', methods=['POST', 'GET'])
 def login():
     if request.method == 'POST':
+        # pega o nome inserido no campo de texto
         loginNome = request.form['nome']
+        # pega a senha inserida no campo de texto
         loginSenha = request.form['senha']
-        if professor1.verifica(loginNome, loginSenha):
+
+        # define o objeto professor como global
+        global professor1
+        # cria o objeto professor
+        professor1 = Professor(loginNome, loginSenha)
+        # verifica se é um professor válido
+        if professor1.verifica():
             return redirect("/redireciona")
+        # se não for, reseta a página retornando a tela de login novamente
         else:
             return redirect("/login")
+    # exibe a tela de login
     return render_template("login.html")
 
+# Página de tarefas
 @app.route('/tarefas', methods=['POST', 'GET'])
 def tarefas():
-    conteudo = aluno1.leTarefa()
+    # pega o conteúdo de tarefa para exbir ao aluno
+    conteudo = Aluno.leTarefa()
     return render_template("tarefas.html", conteudo=conteudo)
 
+# Página de médias
 @app.route("/medias", methods=['POST', 'GET'])
 def medias():
-    conteudo = aluno1.leNota()
+    # pega o conteúdo de notas para exbir ao aluno
+    conteudo = Aluno.leNota()
     return render_template("medias.html", conteudo=conteudo)
 
+# Página de redirecionamento
 @app.route("/redireciona", methods=['POST', 'GET'])
 def redireciona():
     if request.method == 'POST':
         botaoSelecionado = request.form['botao']
+        # se o botão for o da tarefa, chama a tela de tarefasProfessor
         if botaoSelecionado == 'tarefa':
             return redirect("/tarefasProfessor")
+        # se o botão for o de notas, chama a tela de notasProfessor
         elif botaoSelecionado == 'notas': 
             return redirect("/notasProfessor")
+        # se o botão for do tipo voltar retorna a tela de redirecionamento
         elif botaoSelecionado == 'voltar':
             return redirect("/")
+    # exibe a tela de redirecionamento
     return render_template("redirecionaProfessor.html")
 
+# Página de tarefas do professor
 @app.route("/tarefasProfessor", methods=['POST', 'GET'])
 def tarefaProfessor():
     if request.method == 'POST':
         botaoSelecionado = request.form['botao']
-        if botaoSelecionado == 'criaTarefa':
+        # se o botão for o de criar tarefa e os campos estiverem preenchidos, cria a tarefa
+        if botaoSelecionado == 'criaTarefa' and request.form['dataEntrega'] != "" and request.form['nomeAtividade'] != "":
+            # pega a data inserida
             data = request.form['dataEntrega']
+            # pega o nome da tarefa
             nomeTarefa = request.form['nomeAtividade']
+            # pega a descrição da tarefa
             tarefa = request.form['tarefa']
+            # cadastra a tarefa usando o objeto professor
             professor1.cadastroTarefa( data, nomeTarefa, tarefa)
+        # se o botão for do tipo voltar retorna a tela de redirecionamento
         elif botaoSelecionado == 'voltar':
             return redirect("/redireciona")
     return render_template("tarefaProfessor.html")
 
+# Página de notas
 @app.route("/notasProfessor", methods=['POST', 'GET'])
 def notaProfessor():
     if request.method == 'POST':
         botaoSelecionado = request.form['botao']
+        # verifica se o botão de cadastrar nota foi apertado
         if botaoSelecionado == 'cadastraNota':
-            nome = request.form['nomeAluno']
-            dre = request.form['dreAluno']
-            nota1 = request.form['nota1']
-            nota2 = request.form['nota2']
-            lista1 = request.form['lista1']
-            lista2 = request.form['lista2']
-            lista3 = request.form['lista3']
-            lista4 = request.form['lista4']
-            aluno1.postagemNotas(nome, dre, nota1, nota2, lista1, lista2, lista3, lista4)
+            # tenta postar a nota
+            try:
+                nome = request.form['nomeAluno']
+                dre = request.form['dreAluno']
+                aluno1 = Aluno(nome, dre)
+                nota1 = request.form['nota1']
+                nota2 = request.form['nota2']
+                lista1 = request.form['lista1']
+                lista2 = request.form['lista2']
+                lista3 = request.form['lista3']
+                lista4 = request.form['lista4']
+                aluno1.postagemNotas(nota1, nota2, lista1, lista2, lista3, lista4)
+            # em caso de erro reseta a página 
+            except:
+                return redirect("/notasProfessor")
         elif botaoSelecionado == 'voltar':
             return redirect("/redireciona")
     return render_template("notaProfessor.html")
 
-# Coloca o programa para rodar, tirar o "debug=True" qaundo o programa estiver finalizado
-app.run(debug=True)
-#app.run()
+# Função para iniciar o site
+def inicia_site():
+    app.run()
+
+if __name__ == '__main__':
+    # Inicia o site em uma thread separada
+    threading.Thread(target=inicia_site).start()
+    
+    try:
+        # Abre o navegador automaticamente na página local
+        webbrowser.open("http://localhost:5000/")
+    except Exception as e:
+        print("Erro ao abrir o navegador: " + str(e))
--- Tarefa - Calculo de nota/modelo.py
+++ Tarefa - Calculo de nota/modelo.py
@@ -1,15 +1,10 @@
-# A página de modelo terá algumas classes e funções
-
-# Importando as bibliotecas necessarias
-from fpdf import FPDF # Biblioteca que cria arquivos .pdf
-
 # Classe professor (tem as funções referentes ao professor)
-class professor():
-    def __init__(self):
-        pass
-    def verifica(self, nome, senha):
+class Professor():
+    def __init__(self, nome, senha):
         self.nome = nome 
         self.senha = senha
+
+    def verifica(self):
         try:
             with open("./Tarefa - Calculo de nota/logins.txt", "r") as txt:
                 linhas = txt.readlines()
@@ -26,15 +21,15 @@ class professor():
                 arquivo.write("Miceli-1234\nRyan-1234")
             print("O arquivo 'logins.txt' não foi encontrado.")
         return False
-    def cadastro(self, nome, senha): # Possivel cadastro de um novo professor
-        self.nome = nome
-        self.senha = senha
+
+    def cadastro(self): # Possivel cadastro de um novo professor
         try: 
             with open("./Tarefa - Calculo de nota/logins.txt", "a") as txt:
                 txt.write(f"{self.nome} - {self.senha}\n")
         except FileNotFoundError:
             with open("./Tarefa - Calculo de nota/logins.txt", "w") as txt:
                 txt.write(f"{self.nome} - {self.senha}\n")
+
     def cadastroTarefa(self, data, nomeTarefa, tarefa): # Cadastro das tarefas em .txt
         self.data = data
         self.nomeTarefa = nomeTarefa
@@ -42,19 +37,34 @@ class professor():
         with open(f"./Tarefa - Calculo de nota/Tarefas/tarefa.txt", "a") as txt:
             txt.write(f"{self.nomeTarefa}\n\nData de entrega: {self.data}\nTarefa:{self.tarefa}\n\n\n\n")
 # Classe aluno
-class aluno():
-    def __init__(self):
-        pass
-    def postagemNotas(self, nome, dre, t1, t2, l1, l2, l3, l4): # Função que verifica se o aluno existe e cas não exista, salva o nome e dre dele
+class Aluno():
+    #retorna as notas usando o staticmethod por não precisa do método contrutor para exibir esse trecho
+    @staticmethod
+    def leNota():
+        with open("./Tarefa - Calculo de nota/Alunos/aluno.txt", "r") as nota:
+            return nota.read()
+        
+    #retorna as tarefas usando o staticmethod por não precisa do método contrutor para exibir esse trecho    
+    @staticmethod
+    def leTarefa():
+        with open("./Tarefa - Calculo de nota/Tarefas/tarefa.txt", "r") as txt:
+            return txt.read() 
+    
+    # método contrutor da classe aluno
+    def __init__(self, nome, dre):
         self.nome = nome
         self.dre = dre
+
+    def postagemNotas(self,t1, t2, l1, l2, l3, l4): # Função que verifica se o aluno existe e cas não exista, salva o nome e dre dele
         t1 = float(t1)
         t2 = float(t2)
         l1 = float(l1)
         l2 = float(l2)
         l3 = float(l3)
         l4 = float(l4)
+        # faz o cálculo da média
         media = (((t1 + t2) * 0.8) + (((l1 + l2 + l3 + l4) / 4)*0.2))
+        # no bloco abaixo verifica o estado da média do aluno
         if media >= 7:
             situacao = "aprovado"
         elif 7 > media and media >= 3:
@@ -62,11 +72,6 @@ class aluno():
         else:
             situacao = "reprovado"
         
+        # abre o txt de tarefa e adiciona a nota do novo aluno
         with open("./Tarefa - Calculo de nota/Alunos/aluno.txt", "a") as txt:
             txt.write(f"{self.dre} - {situacao}\n")
-    def leNota(self):
-        with open("./Tarefa - Calculo de nota/Alunos/aluno.txt", "r") as nota:
-            return nota.read()
-    def leTarefa(self):
-        with open("./Tarefa - Calculo de nota/Tarefas/tarefa.txt", "r") as txt:
-            return txt.read() 
