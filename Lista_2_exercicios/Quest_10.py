08583975db2bafa85eaaa2fcfc9c20ecdb668c16
Ryan <ryansalesj4@gmail.com>
2023-10-11 14:18:12 -0300

    ADD: Criação das funções para um bom funcionamento da questão 10

--- Lista_2_exercicios/Quest_10.py
+++ Lista_2_exercicios/Quest_10.py
@@ -21,9 +21,11 @@ class bombaCombustivel():
         self.quantidadeCombustivel = quantidadeCombustivel
     def abastecerPorValor(self, valor):
         abastecimento = self.valorLitro * valor
+        bombaCombustivel.alterarQuantidadeCombustivel(abastecimento)
         return abastecimento
     def abastecerPorLitro(self, litro):
         voltaValor = litro / self.valorLitro
+        bombaCombustivel.alterarQuantidadeCombustivel(litro)
         return voltaValor
     def alterarValor(self, valorNovo):
         self.valorLitro = valorNovo
@@ -46,12 +48,33 @@ while True:
     menu()
     acao = input("Digite o número da opção que deseja: ")
     if acao == '1':
-        
+        print("Opções")
+        print("1. Valor em litros")
+        print("2. Valor em dinheiro")
+        opcao = input("Escreva em qual dessas duas opções vai informar o valor: ")
+        if opcao == '1':
+            valor = float(input("Deseja abastecer com quanto? "))
+            petrobras.abastecerPorLitro(valor)
+        elif opcao == '2':
+            valor = float(input("Deseja abastecer com quanto? "))
+            petrobras.abastecerPorValor(valor)
+        else:
+            print("Opção invalida")
     elif acao == '2':
-    
+        print("Deseja alterar para qual valor o combustivel?")
+        valor = float(input("Novo valor: "))
+        petrobras.alterarValor(valor)
     elif acao == '3':
-    
+        print("Deseja alterar o tipo de combustivel?")
+        tipo = input("O novo combustivel é? ")
+        petrobras.alterarCombustivel(tipo)
     elif acao == '4':
-    
+        print("O posto foi abastecido com quantos litros?")
+        litro = float(input("Litro(L): "))
+        petrobras.alterarQuantidadeCombustivel(litro)
     else:
         print("Ação invalida")
+    print("\n")
+    print(f"O tipo de combustivel atualmente é: {petrobras.tipoCombustivel}")
+    print(f"O valor do combustivel atualmente é: {petrobras.valorLitro}")
+    print(f"A quantidade de combustivel no posto atualmente é: {petrobras.quantidadeCombustivel}")
